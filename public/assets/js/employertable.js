5bc55d9bcf773ffbefef5ceade4dd95091975871
mtpetros <peter.almon@gmail.com>
2017-03-21 22:50:26 -0400

    some changes to employertable.js

--- public/assets/js/employertable.js
+++ public/assets/js/employertable.js
@@ -1,30 +1,33 @@
-var data = [
-    {
-        "name":       "Tiger Nixon",
-        "position":   "System Architect",
-        "salary":     "$3,120",
-        "start_date": "2011/04/25",
-        "office":     "Edinburgh",
-        "extn":       "5421"
-    },
-    {
-        "name":       "Garrett Winters",
-        "position":   "Director",
-        "salary":     "$5,300",
-        "start_date": "2011/07/25",
-        "office":     "Edinburgh",
-        "extn":       "8422"
-    }
-];
+
+// This is dummy data for testing. This will be removed in the final version
+// var data = [
+//     {
+//         "name":       "Tiger Nixon",
+//         "position":   "System Architect",
+//         "salary":     "$3,120",
+//         "start_date": "2011/04/25",
+//         "office":     "Edinburgh",
+//         "extn":       "5421"
+//     },
+//     {
+//         "name":       "Garrett Winters",
+//         "position":   "Director",
+//         "salary":     "$5,300",
+//         "start_date": "2011/07/25",
+//         "office":     "Edinburgh",
+//         "extn":       "8422"
+//     }
+// ];
 
 $('#employer-table').DataTable( {
-    data: data,
-    columns: [
-        {data: 'name'},
-        {data: 'position'},
-        {data: 'salary'},
-        {data: 'office'},
-        {data: 'extn'}
-    ]
-    // ajax: '/api/employerdata'
+// more dummy data
+//     data: data,
+//     columns: [
+//         {data: 'name'},
+//         {data: 'position'},
+//         {data: 'salary'},
+//         {data: 'office'},
+//         {data: 'extn'}
+//     ]
+    ajax: '/api/employerdata'
 });
--- public/assets/js/registration.js
+++ public/assets/js/registration.js
@@ -1,10 +1,3 @@
 $('#employer').on('click', function() {
     $('#employer-form').removeClass('hidden');
 });
-
-
-    // function removeClass() {
-    //     document.getElementById("employer-form").classList.remove('hidden');
-    // };
-
-    // document.getElementById("employer").addEventListener('click', removeClass);
