92a77a989c58ce9be33aadc69006de118239f353
mtpetros <peter.almon@gmail.com>
2017-03-22 21:23:53 -0400

    changes to api and html routes

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -4,9 +4,8 @@
 
 // Dependencies
 // =============================================================
-// var path = require("path");
-var registration = require('..models/registration.js');
-var employerTable = require('..models/employerTable.js');
+var path = require("path");
+
 
 // Routes
 // =============================================================
@@ -23,28 +22,7 @@ module.exports = function (app) {
         res.render("registration");
     });
 
-    app.post("/register", function (req, res) {
-        registration.create({
-            name: req.body.name,
-            mode: req.body.mode,
-            phone: req.body.phone,
-            email: req.body.email,
-            street: req.body.street,
-            city: req.body.city,
-            state: req.body.state,
-            skill: req.body.skill,
-            availability: req.body.availability
-        }).success(function (data) {
-            res.render("registered", data.values);
-        });        
-    });
 
-
-    // renders the employer table view
-    app.get("/view/employers", function (req, res) {
-        employerTable.findAll({}).then(function (data) {
-            res.render("employertable", data);
-        });            
-    });        
+        
 
 };
